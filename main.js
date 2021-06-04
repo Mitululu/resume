2813ff4207982deab7b9e2b6f9f9def2fcd922c2
Mitul Saha <mituls@andrew.cmu.edu>
2021-06-03 23:44:36 -0400

    initialized canvas, scene, camera

--- main.js
+++ main.js
@@ -1,6 +1,21 @@
 import './style.css'
 
+import * as THREE from 'three';
+
+const scene = new THREE.Scene();
+
+const camera = new THREE.PerspectiveCamera(75, window.innerWidth/window.innerHeight, 0.1, 1000);
+
+const renderer = new THREE.WebGLRenderer({
+  canvas: document.querySelector('#bg'),
+})
+
+renderer.setPixelRatio(window.devicePixelRatio);
+renderer.setSize(window.innerWidth, window.innerHeight);
+
+renderer.render(scene, camera);
+
 document.querySelector('#app').innerHTML = `
-  <h1>Hello Vite!</h1>
+  <h1>This will be Mitul's resume page!</h1>
   <a href="https://vitejs.dev/guide/features.html" target="_blank">Documentation</a>
 `
